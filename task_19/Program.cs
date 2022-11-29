0925a3e876c56dcff1d81e2a971b8f1397b1a9c4
rena <rena2004.rr@gmail.com>
2022-11-29 16:04:49 +0400

    hometask3

---
+++ task_19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n >= 10000)
+    {
+        int d = n / 10000;
+        int r = n % 10;
+ 
+            if(d == r)
+            {
+                n = n / 10;
+                int d2 = (n / 100) % 10;
+                int r2 = n % 10;
+                if(d2 == r2)
+                    Console.WriteLine("Да");
+            }
+            else 
+            Console.WriteLine("Нет");
+    }
